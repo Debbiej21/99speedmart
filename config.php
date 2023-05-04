a7c99ed2259428a9b2936ca46364a5a35473163c
Debbiej21 <100556173+Debbiej21@users.noreply.github.com>
2023-05-04 20:25:07 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,8 @@ $servername = "database-2.cbyhk7lkaodh.us-east-1.rds.amazonaws.com";
 $username = "aws_user"; 
 $password = "Bait9999"; 
 $dbname = "speed_db"; 
+$bucket = "99speedmartbucket";
+$region = "us-east-1";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
